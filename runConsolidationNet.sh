43a6a0702a4a6462a6dc35c84308f4d767f719c4
root <root@pet-cluster-8.cs.pitt.edu>
2016-02-22 18:18:35 -0500

    Script correction

--- runConsolidationNet.sh
+++ runConsolidationNet.sh
@@ -2,7 +2,7 @@ dir="./Stats/Consolidation/"$1"/"
 
 numOfRuns=30
 
-for (( i=25; i<$numOfRuns; i++ ))
+for (( i=0; i<$numOfRuns; i++ ))
 do
 	output=$dir"net_"$i".log"
 	fio net.fio &> $output
--- runConsolidationNet2.sh
+++ runConsolidationNet2.sh
@@ -2,7 +2,7 @@ dir="./Stats/Consolidation/"$1"/"
 
 numOfRuns=30
 
-for (( i=25; i<$numOfRuns; i++ ))
+for (( i=0; i<$numOfRuns; i++ ))
 do
 	output=$dir"net2_"$i".log"
 	fio net2.fio &> $output
