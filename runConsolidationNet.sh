b83c2dd319c1075c4b8b4c5a25a64cdef89356a0
root <root@pet-cluster-8.cs.pitt.edu>
2016-02-22 18:16:44 -0500

    Native Consolidation

--- runConsolidationNet.sh
+++ runConsolidationNet.sh
@@ -1,8 +1,8 @@
 dir="./Stats/Consolidation/"$1"/"
 
-numOfRuns=25
+numOfRuns=30
 
-for (( i=0; i<$numOfRuns; i++ ))
+for (( i=25; i<$numOfRuns; i++ ))
 do
 	output=$dir"net_"$i".log"
 	fio net.fio &> $output
--- runConsolidationNet2.sh
+++ runConsolidationNet2.sh
@@ -1,8 +1,8 @@
 dir="./Stats/Consolidation/"$1"/"
 
-numOfRuns=25
+numOfRuns=30
 
-for (( i=0; i<$numOfRuns; i++ ))
+for (( i=25; i<$numOfRuns; i++ ))
 do
 	output=$dir"net2_"$i".log"
 	fio net2.fio &> $output
